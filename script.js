4cafb19931401e9236e31311348ed3f68e7b98af
Sefi Cohen <seficohen1@gmail.com>
2022-04-08 17:25:55 +0200

    basic design of product page, added functionallity to display image, buy button save user choice to db

---
+++ script.js
@@ -0,0 +1,51 @@
+// Product page
+const productPageContainer = document.querySelector('.product_page');
+const displayImage = document.querySelector('.display_content img');
+const carouselImages = document.querySelectorAll('.image_carousel li img');
+const selectedProductImage = document.querySelector(
+  '.selected_porduct_image img'
+);
+const colorElements = document.querySelectorAll('.color_option li img');
+const sizeSelectionEl = document.getElementById('size');
+const buyBtn = document.getElementById('buy_btn');
+let selectedColor;
+const productDB = {
+  size: null,
+  color: null,
+};
+const { size, color } = productDB;
+const errorMessage =
+  'You must pick a color for your shirt before continuing with your purchase';
+// carousle display image functionallity
+carouselImages.forEach((img) => {
+  img.addEventListener('mouseover', () => {
+    displayImage.hidden = false;
+    displayImage.src = img.src;
+  });
+  img.addEventListener('mouseout', () => {
+    displayImage.hidden = true;
+  });
+  img.addEventListener('click', () => {
+    selectedProductImage.src = img.src;
+  });
+});
+
+// Color and Size Selection - DB is represented by an object
+let isColorSelected = false;
+
+colorElements.forEach((colorImg) => {
+  colorImg.addEventListener('click', () => {
+    colorImg.classList.add('selected_color');
+    selectedColor = colorImg.id;
+  });
+});
+
+buyBtn.addEventListener('click', () => {
+  if (color == undefined) {
+    document.querySelector('.error_message h4').textContent = errorMessage;
+  }
+  productDB.size = sizeSelectionEl.value;
+  productDB.color = selectedColor;
+  productPageContainer.hidden;
+  console.log(productDB);
+});

83dd0fc658984aeb55acaa405ed8aaf449633ca9
Alicia Cembranos <alicembru15@gmail.com>
2022-05-11 11:06:16 +0200

    object data products and add images to image folder

---
+++ js/utils.js
@@ -0,0 +1,37 @@
+const data = [{
+        id: 1,
+        value: "white-chocolate",
+        name: "Le chocolate blanc",
+        price: "15,00",
+        images: {
+            image1: "./assets/images/white-chocolate/chocolate-white-1.jpg",
+            image2: "./assets/images/white-chocolate/chocolate-white-2.jpg",
+            image3: "./assets/images/white-chocolate/chocolate-white-3.jpg",
+            finalImage: "./assets/images/white-chocolate/chocolate_white_final.jpg",
+        },
+    },
+    {
+        id: 2,
+        value: "milk-chocolate",
+        name: "Le chocolate au lait",
+        price: "20,00",
+        images: {
+            image1: "./assets/images/white-chocolate/chocolate_milk-1.jpg",
+            image2: "./assets/images/white-chocolate/chocolate_milk-2.jpg",
+            image3: "./assets/images/white-chocolate/chocolate_milk-3.jpg",
+            finalImage: "./assets/images/white-chocolate/chocolate_milk_final.jpg",
+        }
+    },
+    {
+        id: 3,
+        value: "dark-chocolate",
+        name: "Le chocolate noir",
+        price: "25,00",
+        images: {
+            image1: "./assets/images/white-chocolate/chocolate_dark-1.jpg",
+            image2: "./assets/images/white-chocolate/chocolate_dark-2.jpg",
+            image3: "./assets/images/white-chocolate/chocolate_dark-3.jpg",
+            finalImage: "./assets/images/white-chocolate/chocolate_dark_final.jpg",
+        }
+    },
+]
